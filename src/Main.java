ad9ce0310645b910b3be315b3e3779773ea9c676
Raul.Bob <raul.bob@endava.com>
2015-05-13 16:24:28 +0300

    I've changed the Main and added a repo

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" --- Hello, mister Bob\n");
+		System.out.println(" !!! Hello, mister Bob ----\n");
 		Student s = new Student (1,"Bob","Dilan","UBC - Cluj Napoca");
 		System.out.println(s);
+
 	}
 
 }
---
+++ src/Repository/Repo.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+public class Repo {
+
+	public ArrayList listOfStudents = new ArrayList();
+	
+}
