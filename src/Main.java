54d414e79638cb7b6b0e690a6ee1989891e89fa3
Raul.Bob <raul.bob@endava.com>
2015-05-13 16:32:15 +0300

    main changed by master

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" 0000 Hello, mister Bob ----\n");
+		System.out.println(" _____Hello, mister Bob ----\n");
 		Student s = new Student (1,"Bob","Dilan","UBC - Cluj Napoca");
 		System.out.println(s);
 
